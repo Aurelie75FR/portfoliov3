dd67353f85e5cdfc7fc0de3450a2b6c60ef4e9c4
Aurelie75FR <aurelielo.vi@gmail.com>
2021-11-16 17:22:00 +0100

    Infrastructure en cours

---
+++ src/Components/UI/Modal.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import classes from "./Modal.module.css";
+
+const Modal = () => {
+  const [modalVisible, setModalVisible] = useState();
+
+  const handleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  return (
+    <>
+      <button className={classes.close-modal} onClick={handleModal}>X</button>
+      {modalVisible && (
+        <div className={classes.overlay}>
+          <div className={classes.modal}>
+            <div className="modal-content">
+              <h2>Message envoyé !</h2>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
